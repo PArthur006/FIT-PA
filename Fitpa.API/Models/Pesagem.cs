9441e68de663fda8f0f8bcb7bea7f1fae87e7759
PArthur006 <parthur.rodrigues06@gmail.com>
2026-07-13 13:21:06 -0300

    feat: adicionar modelo Pesagem com propriedades ID, Data e Peso

---
+++ Fitpa.API/Models/Pesagem.cs
@@ -0,0 +1,9 @@
+namespace Fitpa.API.Models
+{
+    public class Pesagem
+    {
+        public int ID { get; set; }
+        public DateTime Data { get; set; }
+        public decimal Peso { get; set; }
+    }
+}
