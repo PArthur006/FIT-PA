1249021ad349e832f715361e70e687d3a4031a5f
PArthur006 <parthur.rodrigues06@gmail.com>
2026-07-14 12:53:14 -0300

    feat: implementar serviço Pesagem com métodos para CRUD

---
+++ fitpa-web/src/app/services/pesagem.service.ts
@@ -0,0 +1,29 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { Pesagem } from '../models/pesagem.model';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class PesagemService {
+  private apiUrl = 'http://localhost:5142/api/pesagem';
+
+  constructor(private http: HttpClient) {}
+
+  getPesagens(): Observable<Pesagem[]> {
+    return this.http.get<Pesagem[]>(this.apiUrl);
+  }
+
+  registrarPesagem(pesagem: Pesagem): Observable<Pesagem> {
+    return this.http.post<Pesagem>(this.apiUrl, pesagem);
+  }
+
+  editarPesagem(id: number, pesagem: Pesagem): Observable<Pesagem> {
+    return this.http.put<Pesagem>(`${this.apiUrl}/${id}`, pesagem);
+  }
+
+  deletarPesagem(id: number): Observable<void> {
+    return this.http.delete<void>(`${this.apiUrl}/${id}`);
+  }
+}
