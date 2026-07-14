59cd5c2248a788b4bf91ee1a7468f622fae8857b
PArthur006 <parthur.rodrigues06@gmail.com>
2026-07-14 12:53:25 -0300

    feat: adicionar configuração de rotas e componente Pesagem

--- fitpa-web/src/app/app.routes.ts
+++ fitpa-web/src/app/app.routes.ts
@@ -1,3 +1,7 @@
 import { Routes } from '@angular/router';
+import { PesagemComponent } from './components/pesagem/pesagem.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: 'pesagem', component: PesagemComponent },
+    { path: '', redirectTo: '/pesagem', pathMatch: 'full' } 
+];
