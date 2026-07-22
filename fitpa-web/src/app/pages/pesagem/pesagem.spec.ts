af5f66ab2e1d58a08d379a2a4300f8bb32f2ed79
PArthur006 <parthur.rodrigues06@gmail.com>
2026-07-22 11:37:54 -0300

    feat: gerar páginas de login e pesagem com componentes e testes

---
+++ fitpa-web/src/app/pages/pesagem/pesagem.spec.ts
@@ -0,0 +1,22 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { Pesagem } from './pesagem';
+
+describe('Pesagem', () => {
+  let component: Pesagem;
+  let fixture: ComponentFixture<Pesagem>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      imports: [Pesagem],
+    }).compileComponents();
+
+    fixture = TestBed.createComponent(Pesagem);
+    component = fixture.componentInstance;
+    await fixture.whenStable();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
---
+++ fitpa-web/src/app/pages/pesagem/pesagem.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-pesagem',
+  imports: [],
+  templateUrl: './pesagem.html',
+  styleUrl: './pesagem.css',
+})
+export class Pesagem {}
