d434d61095e0b7c39acad4a301a0ab537999ffba
PArthur006 <parthur.rodrigues06@gmail.com>
2026-07-22 12:00:56 -0300

    feat: adicionar páginas de login e pesagem com testes e estilos

--- fitpa-web/src/app/pages/pesagem/pesagem.pages.spec.ts
+++ fitpa-web/src/app/pages/pesagem/pesagem.pages.spec.ts
@@ -1,6 +1,6 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
-import { Pesagem } from './pesagem';
+import { Pesagem } from './pesagem.pages';
 
 describe('Pesagem', () => {
   let component: Pesagem;
