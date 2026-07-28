049078399f4dd0b37c095de3514bd233fb4740ea
PArthur006 <parthur.rodrigues06@gmail.com>
2026-07-28 17:06:26 -0300

    feat: adicionar rota para o componente Rotinas

--- fitpa-web/src/app/app.routes.ts
+++ fitpa-web/src/app/app.routes.ts
@@ -1,9 +1,11 @@
 import { Routes } from '@angular/router';
 import { PesagemComponent } from './components/pesagem/pesagem.component';
 import { LoginComponent } from './pages/login/login';
+import { RotinasComponent } from './pages/treinos/rotinas/rotinas.component';
 
 export const routes: Routes = [
     { path: 'pesagem', component: PesagemComponent },
     { path: 'login', component: LoginComponent },
+    { path: 'nova-rotina', component: RotinasComponent },
     { path: '', redirectTo: '/login', pathMatch: 'full' } 
 ];
