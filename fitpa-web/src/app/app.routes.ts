d3f42de27740ceb4b1b3a8ce1e5eb7276311ce77
PArthur006 <parthur.rodrigues06@gmail.com>
2026-09-22 10:53:51 -0300

    feat: atualizar rotas e layout para incluir componente ListaRotinas

--- fitpa-web/src/app/app.routes.ts
+++ fitpa-web/src/app/app.routes.ts
@@ -3,11 +3,13 @@ import { PesagemComponent } from './components/pesagem/pesagem.component';
 import { LoginComponent } from './pages/login/login';
 import { RotinasComponent } from './pages/treinos/rotinas/rotinas.component';
 import { NovoTreinoComponent } from './pages/treinos/novo-treino/novo-treino.component';
+import { ListaRotinasComponent } from './pages/treinos/lista-rotinas/lista-rotinas.component';
 
 export const routes: Routes = [
     { path: 'pesagem', component: PesagemComponent },
     { path: 'login', component: LoginComponent },
     { path: 'nova-rotina', component: RotinasComponent },
     { path: '', redirectTo: '/login', pathMatch: 'full' },
-    { path: 'novo-treino', component: NovoTreinoComponent } 
+    { path: 'novo-treino', component: NovoTreinoComponent },
+    { path: 'lista-rotinas', loadComponent: () => import('./pages/treinos/lista-rotinas/lista-rotinas.component').then(m => m.ListaRotinasComponent) }
 ];
