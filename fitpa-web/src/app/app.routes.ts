4dddfdd5875c11ec6e373efd47be88ea51334a76
PArthur006 <parthur.rodrigues06@gmail.com>
2026-09-21 23:01:08 -0300

    feat: adicionar rota para o componente NovoTreino

--- fitpa-web/src/app/app.routes.ts
+++ fitpa-web/src/app/app.routes.ts
@@ -2,10 +2,12 @@ import { Routes } from '@angular/router';
 import { PesagemComponent } from './components/pesagem/pesagem.component';
 import { LoginComponent } from './pages/login/login';
 import { RotinasComponent } from './pages/treinos/rotinas/rotinas.component';
+import { NovoTreinoComponent } from './pages/treinos/novo-treino/novo-treino.component';
 
 export const routes: Routes = [
     { path: 'pesagem', component: PesagemComponent },
     { path: 'login', component: LoginComponent },
     { path: 'nova-rotina', component: RotinasComponent },
-    { path: '', redirectTo: '/login', pathMatch: 'full' } 
+    { path: '', redirectTo: '/login', pathMatch: 'full' },
+    { path: 'novo-treino', component: NovoTreinoComponent } 
 ];
